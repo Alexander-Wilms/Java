d61ead9a0c28bfd47b90b4cbd6da8d944b432168
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-12-29 16:49:26 +0100

    CheckPassFail

---
+++ CheckPassFail/src/CheckPassFail.java
@@ -0,0 +1,18 @@
+/*
+ * http://www.ntu.edu.sg/home/ehchua/programming/java/j2a_basicsexercises.html
+ * 2.1
+ * Trying if-else statement.
+ */
+public class CheckPassFail {  // Save as "CheckPassFail.java"
+   public static void main(String[] args) {  // Program entry point
+      int mark = 51;  // Set the value of "mark" here!
+      System.out.println("The mark is " + mark);
+ 
+      if (mark >= 50) {
+         System.out.println("PASS");
+      } else {
+         System.out.println("FAIL");
+      }
+      System.out.println("DONE");
+   }
+}
