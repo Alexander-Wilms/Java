7c478945d691483439849814bb1b88dec4bff234
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-12-30 19:27:24 +0100

    10001st prime

---
+++ 10001st prime/src/TenThousandAndFirstPrime.java
@@ -0,0 +1,28 @@
+
+public class TenThousandAndFirstPrime {
+
+	public static void main(String[] args) {
+		int NoOfPrimes = 0;
+		int  numberToBeTested = 1;
+		int sum = 0;
+		
+		while(NoOfPrimes < 10001) {
+			numberToBeTested++;
+			sum = 0;
+			
+			for(int i = 1; i <= numberToBeTested; i++) {
+				if(numberToBeTested%i == 0)
+					sum++;
+			}
+			
+			if(sum == 2) {
+				
+				NoOfPrimes++;
+				System.out.println("Primzahl " + NoOfPrimes + ": " + numberToBeTested);
+			}
+			
+		}
+		
+		System.out.println(numberToBeTested);
+	}
+}
