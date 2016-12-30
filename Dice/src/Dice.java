fa4b365873f20bf1c08e29d4e6d5b60fd2614c34
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-12-30 17:06:12 +0100

    Dice

--- Dice/src/Dice.java
+++ Dice/src/Dice.java
@@ -5,7 +5,7 @@ public class Dice {
 	public static void main(String[] args) {
 		Random rand = new Random(System.currentTimeMillis());
 		
-		final int COMBINATIONS = 4*6;
+		final int COMBINATIONS = 5*6;
 
 		int NoOfRolls = 100000000;
 		
@@ -21,8 +21,9 @@ public class Dice {
 			int b = Math.abs(rand.nextInt())%6+1;
 			int c = Math.abs(rand.nextInt())%6+1;
 			int d = Math.abs(rand.nextInt())%6+1;
-			int e = a+b+c+d;
-			results[e-1]++;
+			int e = Math.abs(rand.nextInt())%6+1;
+			int f = a+b+c+d+e;
+			results[f-1]++;
 		}
 		
 		long max = 0;
@@ -36,12 +37,10 @@ public class Dice {
 		System.out.println();
 		
 		for(int j = 0; j <= COMBINATIONS-1; j++) {
-			for(int k = 1; k <= (float) results[j]/max*10; k++) {
+			for(int k = 1; k <= (float) results[j]/max*20; k++) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
-		
 	}
-
 }
