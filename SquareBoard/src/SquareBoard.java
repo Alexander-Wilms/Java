1d20ebef1bad6c304c3a4e938652e7ad365d109c
Alexander Wilms <f.alexander.wilms@gmail.com>
2016-12-30 01:06:56 +0100

    SquareBoard

---
+++ SquareBoard/src/SquareBoard.java
@@ -0,0 +1,16 @@
+// http://www.ntu.edu.sg/home/ehchua/programming/java/j2a_basicsexercises.html
+public class SquareBoard {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for(int row = 1; row <= n; row++) {
+			for(int col = 1; col <= n; col++) {
+				System.out.print("# ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
